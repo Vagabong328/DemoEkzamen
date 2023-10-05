052d213a9acbc7817a92e48da23e75c7e88ae603
Vagabong328 <97594467+Vagabong328@users.noreply.github.com>
2023-10-05 13:01:20 +0300

    Add files via upload

---
+++ Skeleton/Rule/Rule/Classes/manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Rule.Classes
+{
+    internal class manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
